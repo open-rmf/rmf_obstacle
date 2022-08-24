137a86cf4776208182604273c9817932f1e87d34
Pranay Shirodkar <39498451+PranayShirodkar@users.noreply.github.com>
2022-08-24 15:07:58 +0800

    uncrustify
    Signed-off-by: Pranay Shirodkar <39498451+PranayShirodkar@users.noreply.github.com>

--- rmf_human_detector/include/rmf_human_detector/YoloDetector.hpp
+++ rmf_human_detector/include/rmf_human_detector/YoloDetector.hpp
@@ -19,57 +19,74 @@
 class YoloDetector
 {
 public:
-    using Obstacle = rmf_obstacle_msgs::msg::Obstacle;
-    using Obstacles = rmf_obstacle_msgs::msg::Obstacles;
+  using Obstacle = rmf_obstacle_msgs::msg::Obstacle;
+  using Obstacles = rmf_obstacle_msgs::msg::Obstacles;
 
-    // User Configurations
-    struct Config
-    {
-        // Camera configurations
-        std::string camera_name;
-        const bool visualize = true;
-        const bool camera_static = true;
-        const float camera_afov;
-        // YoloDetector configurations
-        const std::string nn_filepath;
-        const std::string labels_filepath;
-        const float score_threshold;
-        const float nms_threshold;
-        const float confidence_threshold;
-    };
+  // User Configurations
+  struct Config
+  {
+    // Camera configurations
+    std::string camera_name;
+    const bool visualize = true;
+    const bool camera_static = true;
+    const float camera_afov;
+    // YoloDetector configurations
+    const std::string nn_filepath;
+    const std::string labels_filepath;
+    const float score_threshold;
+    const float nms_threshold;
+    const float confidence_threshold;
+  };
 
-    YoloDetector(std::shared_ptr<Config> config);
-    ~YoloDetector();
-    Obstacles image_cb(const sensor_msgs::msg::Image::ConstSharedPtr &msg);
-    void camera_pose_cb(const geometry_msgs::msg::Transform &msg);
-    void set_image_detections_pub(rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_detections_pub);
+  YoloDetector(std::shared_ptr<Config> config);
+  ~YoloDetector();
+  Obstacles image_cb(const sensor_msgs::msg::Image::ConstSharedPtr& msg);
+  void camera_pose_cb(const geometry_msgs::msg::Transform& msg);
+  void set_image_detections_pub(
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_detections_pub);
 
 private:
 
-    // Yolov5s Constants
-    // Camera image gets scaled to this size before inference
-    const float INPUT_WIDTH = 640.0;
-    const float INPUT_HEIGHT = 640.0;
+  // Yolov5s Constants
+  // Camera image gets scaled to this size before inference
+  const float INPUT_WIDTH = 640.0;
+  const float INPUT_HEIGHT = 640.0;
 
-    // Members
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr _image_detections_pub;
-    std::vector<std::string> _class_list;
-    cv::dnn::Net _net;
-    std::shared_ptr<Config> _config;
-    float _d_param;
-    float _w_param;
-    geometry_msgs::msg::Transform _camera_pose;
+  // Members
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr _image_detections_pub;
+  std::vector<std::string> _class_list;
+  cv::dnn::Net _net;
+  std::shared_ptr<Config> _config;
+  float _d_param;
+  float _w_param;
+  geometry_msgs::msg::Transform _camera_pose;
 
-    // Methods
-    cv::Mat format_yolov5(const cv::Mat &source);
-    std::vector<cv::Mat> detect(cv::Mat &input_image);
-    Obstacles post_process(const cv::Mat &original_image, cv::Mat &image, std::vector<cv::Mat> &detections);
-    cv::Point3d img_coord_to_cam_coord(const cv::Point &centroid, const cv::Mat &original_image);
-    void cam_coord_to_world_coord(Obstacle &obstacle);
-    Obstacles to_rmf_obstacles(const cv::Mat &original_image, const std::vector<int> &final_class_ids, const std::vector<cv::Point> &final_centroids);
-    void drawing(const cv::Mat &original_image, cv::Mat &image, const std::vector<int> &final_class_ids, const std::vector<float> &final_confidences, const std::vector<cv::Rect> &final_boxes, const std::vector<cv::Point> &final_centroids);
-    void draw_label(cv::Mat &input_image, std::string label, int left, int top);
-    void calibrate();
+  // Methods
+  cv::Mat format_yolov5(const cv::Mat& source);
+
+  std::vector<cv::Mat> detect(cv::Mat& input_image);
+
+  Obstacles post_process(const cv::Mat& original_image, cv::Mat& image,
+    std::vector<cv::Mat>& detections);
+
+  cv::Point3d img_coord_to_cam_coord(const cv::Point& centroid,
+    const cv::Mat& original_image);
+
+  void cam_coord_to_world_coord(Obstacle& obstacle);
+
+  Obstacles to_rmf_obstacles(const cv::Mat& original_image,
+    const std::vector<int>& final_class_ids,
+    const std::vector<cv::Point>& final_centroids);
+
+  void drawing(const cv::Mat& original_image, cv::Mat& image,
+    const std::vector<int>& final_class_ids,
+    const std::vector<float>& final_confidences,
+    const std::vector<cv::Rect>& final_boxes,
+    const std::vector<cv::Point>& final_centroids);
+
+  void draw_label(cv::Mat& input_image, std::string label, int left, int top);
+
+  void calibrate();
 
 };
 
--- rmf_human_detector/src/HumanDetector.cpp
+++ rmf_human_detector/src/HumanDetector.cpp
@@ -4,53 +4,55 @@
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <rmf_human_detector/HumanDetector.hpp>
 
-HumanDetector::HumanDetector() : Node("human_detector"), _data(std::make_shared<Data>())
+HumanDetector::HumanDetector()
+: Node("human_detector"), _data(std::make_shared<Data>())
 {
   make_detector();
 
-  _data->_image_detections_pub = this->create_publisher<sensor_msgs::msg::Image>(
-  _data->_image_detections_topic,
-  rclcpp::QoS(10).reliable()
-  );
+  _data->_image_detections_pub =
+    this->create_publisher<sensor_msgs::msg::Image>(
+    _data->_image_detections_topic,
+    rclcpp::QoS(10).reliable()
+    );
   _data->_detector->set_image_detections_pub(_data->_image_detections_pub);
 
   _data->_obstacles_pub = this->create_publisher<Obstacles>(
-  "/rmf_obstacles",
-  rclcpp::SensorDataQoS()
+    "/rmf_obstacles",
+    rclcpp::SensorDataQoS()
   );
 
   _data->_image_sub = this->create_subscription<sensor_msgs::msg::Image>(
-  _data->_camera_image_topic,
-  rclcpp::SensorDataQoS(),
-  [=](const sensor_msgs::msg::Image::ConstSharedPtr &msg)
-  {
-    // perform detections
-    auto rmf_obstacles_msg = _data->_detector->image_cb(msg);
-
-    // populate fields like time stamp, etc
-    for (auto &obstacle : rmf_obstacles_msg.obstacles)
+    _data->_camera_image_topic,
+    rclcpp::SensorDataQoS(),
+    [=](const sensor_msgs::msg::Image::ConstSharedPtr& msg)
     {
-      obstacle.header.stamp = this->get_clock()->now();
-    }
+      // perform detections
+      auto rmf_obstacles_msg = _data->_detector->image_cb(msg);
 
-    // publish rmf_obstacles_msg
-    _data->_obstacles_pub->publish(rmf_obstacles_msg);
-  });
+      // populate fields like time stamp, etc
+      for (auto& obstacle : rmf_obstacles_msg.obstacles)
+      {
+        obstacle.header.stamp = this->get_clock()->now();
+      }
+
+      // publish rmf_obstacles_msg
+      _data->_obstacles_pub->publish(rmf_obstacles_msg);
+    });
 
   _data->_camera_pose_sub = this->create_subscription<tf2_msgs::msg::TFMessage>(
-  _data->_camera_pose_topic,
-  rclcpp::SensorDataQoS(),
-  [=](const tf2_msgs::msg::TFMessage::ConstSharedPtr &msg)
-  {
-    for (auto &transformStamped : msg->transforms)
+    _data->_camera_pose_topic,
+    rclcpp::SensorDataQoS(),
+    [=](const tf2_msgs::msg::TFMessage::ConstSharedPtr& msg)
     {
-      if (transformStamped.header.frame_id == _data->_camera_parent_name
-      && transformStamped.child_frame_id ==  _data->_camera_name)
+      for (auto& transformStamped : msg->transforms)
       {
-        _data->_detector->camera_pose_cb(transformStamped.transform);
+        if (transformStamped.header.frame_id == _data->_camera_parent_name
+        && transformStamped.child_frame_id ==  _data->_camera_name)
+        {
+          _data->_detector->camera_pose_cb(transformStamped.transform);
+        }
       }
-    }
-  });
+    });
 }
 
 void HumanDetector::make_detector()
@@ -61,14 +63,16 @@ void HumanDetector::make_detector()
     "Setting parameter camera_name to %s", _data->_camera_name.c_str()
   );
   _data->_camera_image_topic = _data->_camera_name + "/image_rect";
-  _data->_camera_pose_topic  = _data->_camera_name + "/pose";
-  _data->_camera_info_topic  = _data->_camera_name + "/camera_info";
-  _data->_image_detections_topic  = _data->_camera_name + "/image_detections";
+  _data->_camera_pose_topic = _data->_camera_name + "/pose";
+  _data->_camera_info_topic = _data->_camera_name + "/camera_info";
+  _data->_image_detections_topic = _data->_camera_name + "/image_detections";
 
-  _data->_camera_parent_name = this->declare_parameter("camera_parent_name", "sim_world");
+  _data->_camera_parent_name =
+    this->declare_parameter("camera_parent_name", "sim_world");
   RCLCPP_INFO(
     this->get_logger(),
-    "Setting parameter camera_parent_name to %s", _data->_camera_parent_name.c_str()
+    "Setting parameter camera_parent_name to %s",
+    _data->_camera_parent_name.c_str()
   );
 
   const bool visualize = this->declare_parameter("visualize", true);
@@ -89,13 +93,15 @@ void HumanDetector::make_detector()
     "Setting parameter nn_filepath to %s", nn_filepath.c_str()
   );
 
-  const std::string labels_filepath = this->declare_parameter("labels_filepath", "");
+  const std::string labels_filepath =
+    this->declare_parameter("labels_filepath", "");
   RCLCPP_INFO(
     this->get_logger(),
     "Setting parameter labels_filepath to %s", nn_filepath.c_str()
   );
 
-  const float score_threshold = this->declare_parameter("score_threshold", 0.45);
+  const float score_threshold =
+    this->declare_parameter("score_threshold", 0.45);
   RCLCPP_INFO(
     this->get_logger(),
     "Setting parameter score_threshold to %f", score_threshold
@@ -107,7 +113,8 @@ void HumanDetector::make_detector()
     "Setting parameter nms_threshold to %f", nms_threshold
   );
 
-  const float confidence_threshold = this->declare_parameter("confidence_threshold", 0.25);
+  const float confidence_threshold =
+    this->declare_parameter("confidence_threshold", 0.25);
   RCLCPP_INFO(
     this->get_logger(),
     "Setting parameter confidence_threshold to %f", confidence_threshold
@@ -115,15 +122,17 @@ void HumanDetector::make_detector()
 
   // get one camera_info msg
   sensor_msgs::msg::CameraInfo camera_info;
-  std::shared_ptr<rclcpp::Node> temp_node = std::make_shared<rclcpp::Node>("wait_for_msg_node");
+  std::shared_ptr<rclcpp::Node> temp_node =
+    std::make_shared<rclcpp::Node>("wait_for_msg_node");
   rclcpp::wait_for_message(camera_info, temp_node, _data->_camera_info_topic);
 
   // calculate camera fov
   float f_x = camera_info.p[0];
-  float fov_x = 2 * atan2( camera_info.width, (2*f_x) );
+  float fov_x = 2 * atan2(camera_info.width, (2*f_x) );
 
   // make detector
-  std::shared_ptr<YoloDetector::Config> config = std::make_shared<YoloDetector::Config>(
+  std::shared_ptr<YoloDetector::Config> config =
+    std::make_shared<YoloDetector::Config>(
     YoloDetector::Config{
       _data->_camera_name,
       visualize,
@@ -135,7 +144,7 @@ void HumanDetector::make_detector()
       nms_threshold,
       confidence_threshold,
     }
-  );
+    );
 
   _data->_detector = std::make_shared<YoloDetector>(config);
 }
@@ -144,7 +153,7 @@ HumanDetector::~HumanDetector()
 {
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
   std::cout << "Starting HumanDetector node" << std::endl;
